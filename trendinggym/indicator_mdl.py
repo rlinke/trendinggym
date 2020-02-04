c4d1e432b65c774eea60c54ff90f13fdb5c19d74
mkuhn <marcelkuhn87@gmx.de>
2020-02-04 22:46:05 +0100

    small changes

--- trendinggym/indicator_mdl.py
+++ trendinggym/indicator_mdl.py
@@ -14,7 +14,7 @@ import numpy as np
 filepath = "./data/test_stock.csv"
     
 ticker_list = ["^GSPC"]
-
+list_of_stocks = ticker_list
 df_read = pd.read_csv(filepath, index_col=0, header=0)
 
 # Clean NaN values
@@ -22,7 +22,7 @@ df_read = ta.utils.dropna(df_read)
 
 def create_indicators(df_read, list_of_stocks):
     
-    df=pd.DataFrame()
+    df_x=pd.DataFrame()
     for stock in list_of_stocks:
         
         # Extract data
@@ -32,14 +32,15 @@ def create_indicators(df_read, list_of_stocks):
         s_vol = pd.Series(list(df_read[[s for s in df_read.columns if 'Volume_'+stock in s]].values))
     
         # Calc indicators
-        df[stock+'_macd'] = ta.trend.macd(s_close, n_slow = 26, n_fast = 12 ,fillna = True)       
-        df[stock+'_roc'] = ta.momentum.roc(s_close, n = 12, fillna = True)        
-        df[stock+'_wr'] = ta.momentum.wr(s_high, s_low, s_close, lbp = 14, fillna = True)        
-        df[stock+'_mov'] = ta.volume.ease_of_movement(s_high, s_low, s_vol, n = 14, fillna = True)        
-        df[stock+'_rsi'] = ta.momentum.rsi(s_close, n = 14, fillna = True)
+        df_x[stock+'_macd'] = ta.trend.macd(s_close, n_slow = 26, n_fast = 12 ,fillna = False)       
+        df_x[stock+'_roc'] = ta.momentum.roc(s_close, n = 12, fillna = False)        
+        df_x[stock+'_wr'] = ta.momentum.wr(s_high, s_low, s_close, lbp = 14, fillna = False)        
+        df_x[stock+'_mov'] = ta.volume.ease_of_movement(s_high, s_low, s_vol, n = 14, fillna = False)        
+        df_x[stock+'_rsi'] = ta.momentum.rsi(s_close, n = 14, fillna = False)
         
+        df_x.index = df_read.index
     
-    df = df.dropna(how='any')
+    #df = df.dropna(how='any')
     
     if False:
         plt.figure()
@@ -47,39 +48,30 @@ def create_indicators(df_read, list_of_stocks):
         
         fig, axs = plt.subplots(5)
         fig.suptitle('Indicators')
-        axs[0].plot(df_read.index, df[stock+'_rsi'].values)
-        axs[1].plot(df_read.index, df[stock+'_mov'].values)
-        axs[2].plot(df_read.index, df[stock+'_wr'].values)
-        axs[3].plot(df_read.index, df[stock+'_roc'].values)
-        axs[4].plot(df_read.index, df[stock+'_macd'].values)
+        axs[0].plot(df_read.index, df_x[stock+'_rsi'].values)
+        axs[1].plot(df_read.index, df_x[stock+'_mov'].values)
+        axs[2].plot(df_read.index, df_x[stock+'_wr'].values)
+        axs[3].plot(df_read.index, df_x[stock+'_roc'].values)
+        axs[4].plot(df_read.index, df_x[stock+'_macd'].values)
   
 
-
-forecast = 7
-
-df_y = pd.DataFrame(columns=['+7','+6','+5','+4','+3','+2','+1'])
-
-df_y = pd.DataFrame()
-
-df_y['+7'] = s_close[7:]
-df_y['+6'] = s_close[6:]
-df_y['+5'] = s_close[5:]
-df_y['+4'] = s_close[4:]
-df_y['+3'] = s_close[3:]
-df_y['+2'] = s_close[2:]
-df_y['+1'] = s_close[1:]
-
-#for index in range(0,len(s_close)):
-#
-#    if index == len(s_close)-forecast:
-#        break
-#    
-#    df_y.insert(index, '+7', s_close[index+7][0])  
-#    
-#    print(s_close[index+7][0])
+    # forecast = 7: t0 = first index, t_end = real index - 7 - 
+    df_y = pd.DataFrame()
+    
+    df_y['+7'] = np.concatenate(s_close[7:].values)
+    df_y['+6'] = np.concatenate(s_close[6:-1].values)
+    df_y['+5'] = np.concatenate(s_close[5:-2].values)
+    df_y['+4'] = np.concatenate(s_close[4:-3].values)
+    df_y['+3'] = np.concatenate(s_close[3:-4].values)
+    df_y['+2'] = np.concatenate(s_close[2:-5].values)
+    df_y['+1'] = np.concatenate(s_close[1:-6].values)
+    
+    df_y.index = df_read.index[:-7]
+    
+    # align index and clean data 
 
     
-    
+    # save training data
         
 if __name__ == '__main__':
 	create_indicators(df_read, ticker_list)
