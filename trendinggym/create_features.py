fca5c09db31107c3e56b7ba4df4393fbe4db3f30
mkuhn <marcelkuhn87@gmx.de>
2020-02-17 23:23:03 +0100

    preprocessing for lstm

--- trendinggym/create_features.py
+++ trendinggym/create_features.py
@@ -29,34 +29,36 @@ def create_indicators(df_read, list_of_stocks, path):
     for stock in list_of_stocks:
         
         # Extract data - https://technical-analysis-library-in-python.readthedocs.io/en/latest/ta.html
-        s_close = pd.Series(list(df_read[[s for s in df_read.columns if 'Close_'+stock in s and 'Adj' not in s]].values))
-        s_low = pd.Series(list(df_read[[s for s in df_read.columns if 'Low_'+stock in s]].values))
-        s_high = pd.Series(list(df_read[[s for s in df_read.columns if 'High_'+stock in s]].values))
-        s_vol = pd.Series(list(df_read[[s for s in df_read.columns if 'Volume_'+stock in s]].values))
+        s_close = pd.Series(df_read[[s for s in df_read.columns if 'Close_'+stock in s and 'Adj' not in s]].values[:,0])
+        s_low = pd.Series(df_read[[s for s in df_read.columns if 'Low_'+stock in s]].values[:,0])
+        s_high = pd.Series(df_read[[s for s in df_read.columns if 'High_'+stock in s]].values[:,0])
+        s_vol = pd.Series(df_read[[s for s in df_read.columns if 'Volume_'+stock in s]].values[:,0])
     
         # Calc indicators
         df_x[stock+'_macd'] = ta.trend.macd(s_close, n_slow = 26, n_fast = param ,fillna = False)       
-        df_x[stock+'_roc'] = ta.momentum.roc(s_close, n = 12, fillna = False)        
-        df_x[stock+'_wr'] = ta.momentum.wr(s_high, s_low, s_close, lbp = 14, fillna = False)        
+        df_x[stock+'_roc'] = ta.momentum.roc(s_close, n = 12, fillna = False)   
+        df_x[stock+'_wr'] = ta.momentum.wr(s_high, s_low, s_close, lbp = 14, fillna = False)       
         df_x[stock+'_mov'] = ta.volume.ease_of_movement(s_high, s_low, s_vol, n = 14, fillna = False)        
         df_x[stock+'_rsi'] = ta.momentum.rsi(s_close, n = 14, fillna = False)
+        df_x[stock+'_close'] = s_close.values
+
         
         df_x.index = df_read.index  
 
     # forecast = 7: t0 = first index, t_end = real index - 7 - 
     df_y = pd.DataFrame()
     
-    df_y['+7'] = np.concatenate(s_close[7:].values)
-    df_y['+6'] = np.concatenate(s_close[6:-1].values)
-    df_y['+5'] = np.concatenate(s_close[5:-2].values)
-    df_y['+4'] = np.concatenate(s_close[4:-3].values)
-    df_y['+3'] = np.concatenate(s_close[3:-4].values)
-    df_y['+2'] = np.concatenate(s_close[2:-5].values)
-    df_y['+1'] = np.concatenate(s_close[1:-6].values)
-    
+    df_y['+7'] = s_close[7:].values
+    df_y['+6'] = s_close[6:-1].values
+    df_y['+5'] = s_close[5:-2].values
+    df_y['+4'] = s_close[4:-3].values
+    df_y['+3'] = s_close[3:-4].values
+    df_y['+2'] = s_close[2:-5].values
+    df_y['+1'] = s_close[1:-6].values
+        
     df_y.index = df_read.index[:-7]
     
-    # align index and clean data     
+    # align index and clean data   
     df_data_save = pd.concat([df_x, df_y.reindex(df_x.index)], axis=1)
     df_data_save = df_data_save.tail(len(df_data_save)-param-1)
     
--- trendinggym/predict_mdl.py
+++ trendinggym/predict_mdl.py
@@ -20,8 +20,30 @@ init_train_data = pd.Timestamp("2016-01-01")
 
 df_read = pd.read_csv(path, index_col=0, header=0)
 
+def preprocessing(df):
+    
+    df_train = df.dropna()
+    
+    df_close_temp = df_train[[s for s in df_train.columns if 'close' in s]]
+    
+    df_y_train = pd.DataFrame()
+    
+    df_y_train['+7'] = df_train['+7'].values - df_close_temp.values[:,0]
+    df_y_train['+6'] = df_train['+6'].values - df_close_temp.values[:,0]
+    df_y_train['+5'] = df_train['+5'].values - df_close_temp.values[:,0]
+    df_y_train['+4'] = df_train['+4'].values - df_close_temp.values[:,0]
+    df_y_train['+3'] = df_train['+3'].values - df_close_temp.values[:,0]
+    df_y_train['+2'] = df_train['+2'].values - df_close_temp.values[:,0]
+    df_y_train['+1'] = df_train['+1'].values - df_close_temp.values[:,0]
+    
+    df_y_train = (df_y_train > 0) * 1
+    
+    df_x_train = df[[col for col in df.columns if col not in df_y_train.columns]]
+    
+    return df_x_train, df_y_train,
+    
 
-def create_dataset(dataset, look_back=1):
+def create_lstm_dataset(dataset, look_back=1):
 	dataX, dataY = [], []
 	for i in range(len(dataset)-look_back-1):
 		a = dataset[i:(i+look_back), 0]
@@ -30,19 +52,33 @@ def create_dataset(dataset, look_back=1):
 	return np.array(dataX), np.array(dataY)
 
 
+def mdl_lstm(look_back, features):
+    
+    n_first_layer = 128
+    n_second_layer = 64
+    n_output_layer = 7
+    
+    #Build the model
+    model = Sequential()
+    model.add(LSTM(n_first_layer, input_shape=(look_back,1), return_sequences=True))
+    model.add(LSTM(n_second_layer,input_shape=(n_first_layer,1), dropout=0.2, recurrent_dropout=0.2))
+    model.add(Dense(n_output_layer, activation='softmax'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    
+    return model
 
-# Build the model
-model = Sequential()
+df_X, df_Y = preprocessing(df_read)
 
-# I arbitrarily picked the output dimensions as 4
-model.add(LSTM(4, input_dim = input_dim, input_length = input_length))
-# The max output value is > 1 so relu is used as final activation.
-model.add(Dense(output_dim, activation='relu'))
+look_back = 1
+trainX, trainY = create_lstm_dataset(train, look_back)
 
-model.compile(loss='mean_squared_error',
-              optimizer='adam',
-              metrics=['accuracy'])
+trainX = numpy.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
 
-history = model.fit(X_train, y_train,
-              batch_size=7, nb_epoch=3,
-              verbose = 1)
+history = model.fit(X_train,y_train,
+                    epochs=200,
+                    validation_data=(X_validate,y_validate),
+                    shuffle=True,
+                    batch_size=2, 
+                    verbose=2)
+
+
